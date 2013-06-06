e9948f55d07da0b2a25dc6567e871b666571570c
Sébastien Crozet <developer@crozet.re>
2013-06-06 22:57:07 +0000

    Add rotation wrt a point.

--- src/traits/rotation.rs
+++ src/traits/rotation.rs
@@ -1,3 +1,5 @@
+use traits::translation::Translation;
+
 /// Trait of object which represent a rotation, and to wich new rotations can
 /// be appended. A rotation is assumed to be an isomitry without translation
 /// and without reflexion.
@@ -12,4 +14,22 @@ pub trait Rotation<V>
 
   /// In-place version of `rotated`.
   fn rotate(&mut self, &V);
+}
+
+/**
+ * Applies a rotation centered on a specific point.
+ *
+ *   - `m`:       the object to be rotated.
+ *   - `ammount`: the rotation to apply.
+ *   - `point`:   the center of rotation.
+ */
+pub fn rotate_wrt_point<M: Rotation<AV> + Translation<LV>, LV: Neg<LV>, AV>
+       (m: &M, ammount: &AV, center: &LV) -> M
+{
+  let mut res = m.translated(&-center);
+
+  res.rotate(ammount);
+  res.translate(center);
+
+  res
 }
