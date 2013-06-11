fb20ffdf8b545668055b1deac13226229ea72038
Sébastien Crozet <developer@crozet.re>
2013-06-11 11:03:16 +0200

    Add automatic implementation of VectorSpace.

--- src/traits/vector_space.rs
+++ src/traits/vector_space.rs
@@ -6,7 +6,11 @@ use traits::workarounds::scalar_op::{ScalarMul, ScalarDiv};
 /// structure, the elements of which have addition, substraction, negation,
 /// scalar multiplication (the scalar being a element of a `DivisionRing`), and
 /// has a distinct element (`Zero`) neutral wrt the addition.
-pub trait VectorSpace<N: DivisionRing>
+pub trait VectorSpace<N>
 : Sub<Self, Self> + Add<Self, Self> + Neg<Self> + Zero +
   ScalarMul<N> + ScalarDiv<N>
 { }
+
+impl<V: Sub<V, V> + Add<V, V> + Neg<V> + Zero +
+        ScalarMul<N> + ScalarDiv<N>,
+     N: DivisionRing> VectorSpace<N> for V;
