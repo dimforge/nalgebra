5187a1a73f2fd92d8b46ebc02729f5b8f36c3238
Sébastien Crozet <developer@crozet.re>
2013-08-15 10:41:47 +0200

    Add `Round` impl for vectors.

--- src/traits/vec_cast.rs
+++ src/traits/vec_cast.rs
@@ -1,4 +1,4 @@
-/// Trait of vectors which can be converted to another matrix. Used to change the type of a vector
+/// Trait of vectors which can be converted to another vector. Used to change the type of a vector
 /// components.
 pub trait VecCast<V> {
     /// Converts `v` to have the type `V`.
