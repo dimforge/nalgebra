ef4a52d53bb7490a498b8ba039734a32469e629e
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-03-29 13:44:28 +0200

    Remove unused feature unboxed_closures

--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,6 @@ Feel free to add your project to this list if you happen to use **nalgebra**!
 #![deny(unused_qualifications)]
 #![deny(unused_results)]
 #![warn(missing_docs)]
-#![feature(unboxed_closures)]
 #![feature(core)]
 #![cfg_attr(test, feature(test))]
 #![doc(html_root_url = "http://nalgebra.org/doc")]
