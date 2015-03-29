70b4def5cce610d5c85acac16b3a081781c6a259
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-03-29 13:20:52 +0200

    Stop using unstable std_misc feature
    It was only used for `Float::{max_value, min_value}` in non-necessary places,
    so for now this helps stabilization for Rust 1.0.

--- src/lib.rs
+++ src/lib.rs
@@ -84,7 +84,6 @@ Feel free to add your project to this list if you happen to use **nalgebra**!
 #![warn(missing_docs)]
 #![feature(unboxed_closures)]
 #![feature(core)]
-#![feature(std_misc)]
 #![feature(test)]
 #![doc(html_root_url = "http://nalgebra.org/doc")]
 
