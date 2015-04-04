9d428be8b4606675d82627343f6aad1cafbe5965
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-04-04 16:57:59 +0200

    Rename frac_2_sqrtpi -> frac_2_sqrt_pi

--- src/traits/structure.rs
+++ src/traits/structure.rs
@@ -36,7 +36,7 @@ pub trait BaseFloat: Float + Cast<f64> + BaseNum {
     /// 2.0 / pi.
     fn frac_2_pi() -> Self;
     /// 2.0 / sqrt(pi).
-    fn frac_2_sqrtpi() -> Self;
+    fn frac_2_sqrt_pi() -> Self;
 
     /// Euler's number.
     fn e() -> Self;
@@ -424,7 +424,7 @@ macro_rules! impl_base_float(
             }
 
             /// 2.0 / sqrt(pi).
-            fn frac_2_sqrtpi() -> $n {
+            fn frac_2_sqrt_pi() -> $n {
                 $n::consts::FRAC_2_SQRT_PI
             }
 
