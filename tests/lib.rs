ec65944bbd8b9b8900c43b4e01d4b2214516f0e6
Dzmitry Malyshau <kvark@mozilla.com>
2017-07-25 11:19:18 -0400

    Mint vector tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -7,6 +7,8 @@ extern crate num_traits as num;
 extern crate serde_json;
 #[cfg(feature = "abomonation-serialize")]
 extern crate abomonation;
+#[cfg(feature = "mint")]
+extern crate mint;
 extern crate rand;
 extern crate alga;
 extern crate nalgebra as na;
