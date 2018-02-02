39d20306f11b1fa4dd2af9ef5ec0604126fcd556
Sébastien Crozet <sebastien@crozet.re>
2018-02-02 12:26:07 +0100

    Add symmetric quadratic form computation.

--- tests/core/blas.rs
+++ tests/core/blas.rs
@@ -52,4 +52,23 @@ quickcheck! {
 
         relative_eq!(a1.lower_triangle(), a2)
     }
+
+    fn quadform_symm(n: usize, alpha: f64, beta: f64) -> bool {
+        let n = cmp::max(1, cmp::min(n, 50));
+        let lhs         = DMatrix::<f64>::new_random(6, n);
+        let mut mid     = DMatrix::<f64>::new_random(n, n);
+        let mut res     = DMatrix::new_random(6, 6);
+        let mut scratch = Vector6::zeros();
+
+        mid.fill_upper_triangle_with_lower_triangle();
+
+        let expected = &res * beta + &lhs * &mid * lhs.transpose() * alpha;
+
+        res.quadform_symm(&mut scratch, alpha, &lhs, &mid, beta);
+        res.fill_upper_triangle_with_lower_triangle();
+
+        println!("{}{}", res, expected);
+
+        relative_eq!(res.lower_triangle(), expected.lower_triangle(), epsilon = 1.0e-7)
+    }
 }
