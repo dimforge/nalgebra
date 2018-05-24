8e542d6cc2d581f23712941ff5670c9f56ce87a4
sebcrozet <developer@crozet.re>
2018-05-24 18:44:26 +0200

    Reduce the number of jobs.

--- ci/test.sh
+++ ci/test.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
 if [ -z NO_STD ]; then
-    cargo test --verbose ${CARGO_FEATURES};
-
-    if [ -z CARGO_FEATURES ]; then
-        export CARGO_FEATURE_SYSTEM_NETLIB=1 CARGO_FEATURE_EXCLUDE_LAPACKE=1 CARGO_FEATURE_EXCLUDE_CBLAS=1
-        cd nalgebra-lapack; cargo test --verbose;
-    fi
+    cargo test --verbose;
+    cargo test --verbose "debug arbitrary mint serde-serialize abomonation-serialize";
+    cd nalgebra-lapack; cargo test --verbose;
 fi
