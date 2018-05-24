4eaa087b63da3885b164fa7a921b70cb76fdcaba
sebcrozet <developer@crozet.re>
2018-05-24 23:56:47 +0200

    Dedicate a specific CI job to nalgebra-lapack + fix Xargo build.

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,10 @@
 set -ev
 
 if [ -z "$NO_STD" ]; then
-    cargo test --verbose;
-    cargo test --verbose "debug arbitrary mint serde-serialize abomonation-serialize";
-    cd nalgebra-lapack; cargo test --verbose;
+    if [ -z "$LAPACK" ]; then
+        cargo test --verbose;
+        cargo test --verbose "debug arbitrary mint serde-serialize abomonation-serialize";
+    else
+        cd nalgebra-lapack; cargo test --verbose;
+    fi
 fi
