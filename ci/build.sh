681e71d784d64e1b037e31a40dd69c032a38f73d
sebcrozet <developer@crozet.re>
2018-05-24 23:56:47 +0200

    Dedicate a specific CI job to nalgebra-lapack + fix Xargo build.

--- ci/build.sh
+++ ci/build.sh
@@ -3,16 +3,26 @@
 set -ev
 
 if [ -z "$NO_STD" ]; then
-    cargo build --verbose;
-    cargo build --verbose --features "arbitrary";
-    cargo build --verbose --features "mint";
-    cargo build --verbose --features "alloc";
-    cargo build --verbose --features "serde-serialize";
-    cargo build --verbose --features "abomonation-serialize";
-    cargo build --verbose --features "debug";
-    cargo build --verbose --features "debug arbitrary mint serde-serialize abomonation-serialize";
+    if [ -z "$LAPACK" ]; then
+        cargo build --verbose -p nalgebra;
+        cargo build --verbose -p nalgebra --features "arbitrary";
+        cargo build --verbose -p nalgebra --features "mint";
+        cargo build --verbose -p nalgebra --features "alloc";
+        cargo build --verbose -p nalgebra --features "serde-serialize";
+        cargo build --verbose -p nalgebra --features "abomonation-serialize";
+        cargo build --verbose -p nalgebra --features "debug";
+        cargo build --verbose -p nalgebra --features "debug arbitrary mint serde-serialize abomonation-serialize";
+    else
+        cargo build -p nalgebra-lapack;
+    fi
 else
+    if [ "$CARGO_FEATURES" == "alloc" ]; then
+        cat << EOF > Xargo.toml
+[target.x86_64-unknown-linux-gnu.dependencies]
+alloc = {}
+EOF
+    fi
     rustup component add rust-src
-    cargo install xargo;
+    cargo install xargo
     xargo build --verbose --no-default-features --target=x86_64-unknown-linux-gnu --features "${CARGO_FEATURES}";
 fi
