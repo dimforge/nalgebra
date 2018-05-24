01fbf1cce9b9e84602ee84eb7c3649ce92d2786c
sebcrozet <developer@crozet.re>
2018-05-24 18:44:26 +0200

    Reduce the number of jobs.

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
 if [ -z NO_STD ]; then
-    cargo build --verbose --features "${CARGO_FEATURES}";
+    cargo build --verbose;
+    cargo build --verbose --features "arbitrary";
+    cargo build --verbose --features "mint";
+    cargo build --verbose --features "alloc";
+    cargo build --verbose --features "serde-serialize";
+    cargo build --verbose --features "abomonation-serialize";
+    cargo build --verbose --features "debug";
+    cargo build --verbose --features "debug arbitrary mint serde-serialize abomonation-serialize";
 else
     cargo install xargo;
     xargo build --verbose --no-default-features --target=x86_64-unknown-linux-gnu --features "${CARGO_FEATURES}";
