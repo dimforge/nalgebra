2b6ec3fb35d721077ee13793e5522630a2850537
sebcrozet <developer@crozet.re>
2018-09-13 06:06:04 +0200

    Fix compilation of test when arbitrary is enabled but not `debug`.

--- ci/test.sh
+++ ci/test.sh
@@ -5,6 +5,7 @@ set -ev
 if [ -z "$NO_STD" ]; then
     if [ -z "$LAPACK" ]; then
         cargo test --verbose;
+        cargo test --verbose "arbitrary";
         cargo test --verbose "debug arbitrary mint serde-serialize abomonation-serialize";
     else
         cd nalgebra-lapack; cargo test --verbose;
