5869c9ce1308038da16137bc0c12eeae11081858
sebcrozet <developer@crozet.re>
2018-09-22 20:16:43 +0200

    Add more 2D transformation constructors.

--- nalgebra-glm/src/gtx/transform.rs
+++ nalgebra-glm/src/gtx/transform.rs
@@ -1,4 +1,4 @@
-use na::{Real, Unit, Rotation3, Matrix4, U3, U4};
+use na::{Real, Unit, Rotation2, Rotation3, Matrix3, Matrix4, U2, U3, U4};
 
 use traits::Number;
 use aliases::{Vec, Mat};
@@ -16,4 +16,20 @@ pub fn scaling<N: Number>(v: &Vec<N, U3>) -> Mat<N, U4, U4> {
 /// A 4 * 4 translation matrix created from the scaling factor on each axis.
 pub fn translation<N: Number>(v: &Vec<N, U3>) -> Mat<N, U4, U4> {
     Matrix4::new_translation(v)
+}
+
+
+/// A rotation 3 * 3 matrix created from an angle expressed in radians.
+pub fn rotation2d<N: Real>(angle: N) -> Mat<N, U3, U3> {
+    Rotation2::new(angle).to_homogeneous()
+}
+
+/// A 3 * 3 scale matrix created from a vector of 2 components.
+pub fn scaling2d<N: Number>(v: &Vec<N, U2>) -> Mat<N, U3, U3> {
+    Matrix3::new_nonuniform_scaling(v)
+}
+
+/// A 3 * 3 translation matrix created from the scaling factor on each axis.
+pub fn translation2d<N: Number>(v: &Vec<N, U2>) -> Mat<N, U3, U3> {
+    Matrix3::new_translation(v)
 }
