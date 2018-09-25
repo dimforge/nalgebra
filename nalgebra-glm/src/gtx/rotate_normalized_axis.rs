96a2a568009e4301f1f1bc0a2f1da1062689724c
Bruce Mitchener <bruce.mitchener@gmail.com>
2018-09-25 10:07:53 +0700

    Use 3 space indents to avoid triggering syntax highlighting.

--- nalgebra-glm/src/gtx/rotate_normalized_axis.rs
+++ nalgebra-glm/src/gtx/rotate_normalized_axis.rs
@@ -5,9 +5,9 @@ use aliases::{Qua, TMat4, TVec3};
 /// Builds a rotation 4 * 4 matrix created from a normalized axis and an angle.
 ///
 /// # Parameters
-///     * `m` - Input matrix multiplied by this rotation matrix.
-///     * `angle` - Rotation angle expressed in radians.
-///     * `axis` - Rotation axis, must be normalized.
+///   * `m` - Input matrix multiplied by this rotation matrix.
+///   * `angle` - Rotation angle expressed in radians.
+///   * `axis` - Rotation axis, must be normalized.
 pub fn rotate_normalized_axis<N: Real>(m: &TMat4<N>, angle: N, axis: &TVec3<N>) -> TMat4<N> {
     m * Rotation3::from_axis_angle(&Unit::new_unchecked(*axis), angle).to_homogeneous()
 }
@@ -15,9 +15,9 @@ pub fn rotate_normalized_axis<N: Real>(m: &TMat4<N>, angle: N, axis: &TVec3<N>) 
 /// Rotates a quaternion from a vector of 3 components normalized axis and an angle.
 ///
 /// # Parameters
-///     * `q` - Source orientation
-///     * `angle` - Angle expressed in radians.
-///     * `axis` - Normalized axis of the rotation, must be normalized.
+///   * `q` - Source orientation
+///   * `angle` - Angle expressed in radians.
+///   * `axis` - Normalized axis of the rotation, must be normalized.
 pub fn quat_rotate_normalized_axis<N: Real>(q: &Qua<N>, angle: N, axis: &TVec3<N>) -> Qua<N> {
     q * UnitQuaternion::from_axis_angle(&Unit::new_unchecked(*axis), angle).unwrap()
 }
