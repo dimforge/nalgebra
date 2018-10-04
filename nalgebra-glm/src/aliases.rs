a01fde5699453ac57f62ea510e968bf06e88fa0d
Bruce Mitchener <bruce.mitchener@gmail.com>
2018-10-04 20:49:01 +0700

    Add more doc linking and some doc tests.

--- nalgebra-glm/src/aliases.rs
+++ nalgebra-glm/src/aliases.rs
@@ -5,19 +5,151 @@ use na::{MatrixMN, VectorN, Vector1, Vector2, Vector3, Vector4,
          Quaternion};
 
 /// A matrix with components of type `N`. It has `R` rows, and `C` columns.
+///
+/// # See also:
+///
+/// * [`TMat2`](type.TMat2.html)
+/// * [`TMat2x2`](type.TMat2x2.html)
+/// * [`TMat2x3`](type.TMat2x3.html)
+/// * [`TMat2x4`](type.TMat2x4.html)
+/// * [`TMat3`](type.TMat3.html)
+/// * [`TMat3x2`](type.TMat3x2.html)
+/// * [`TMat3x3`](type.TMat3x3.html)
+/// * [`TMat3x4`](type.TMat3x4.html)
+/// * [`TMat4`](type.TMat4.html)
+/// * [`TMat4x2`](type.TMat4x2.html)
+/// * [`TMat4x3`](type.TMat4x3.html)
+/// * [`TMat4x4`](type.TMat4x4.html)
 pub type TMat<N, R, C> = MatrixMN<N, R, C>;
 /// A column vector with components of type `N`. It has `D` rows (and one column).
+///
+/// # See also:
+///
+/// * [`TVec1`](type.TVec1.html)
+/// * [`TVec2`](type.TVec2.html)
+/// * [`TVec3`](type.TVec3.html)
+/// * [`TVec4`](type.TVec4.html)
 pub type TVec<N, R> = VectorN<N, R>;
 /// A quaternion with components of type `N`.
 pub type Qua<N> = Quaternion<N>;
 
 /// A 1D vector with components of type `N`.
+///
+/// # See also:
+///
+/// ## Constructors:
+///
+/// * [`make_vec1`](fn.make_vec1.html)
+/// * [`vec1`](fn.vec1.html)
+/// * [`vec2_to_vec1`](fn.vec2_to_vec1.html)
+/// * [`vec3_to_vec1`](fn.vec3_to_vec1.html)
+/// * [`vec4_to_vec1`](fn.vec4_to_vec1.html)
+///
+/// ## Related types:
+///
+/// * [`BVec1`](type.BVec1.html)
+/// * [`DVec1`](type.DVec1.html)
+/// * [`IVec1`](type.IVec1.html)
+/// * [`I16Vec1`](type.I16Vec1.html)
+/// * [`I32Vec1`](type.I32Vec1.html)
+/// * [`I64Vec1`](type.I64Vec1.html)
+/// * [`I8Vec1`](type.I8Vec1.html)
+/// * [`TVec`](type.TVec.html)
+/// * [`UVec1`](type.UVec1.html)
+/// * [`U16Vec1`](type.U16Vec1.html)
+/// * [`U32Vec1`](type.U32Vec1.html)
+/// * [`U64Vec1`](type.U64Vec1.html)
+/// * [`U8Vec1`](type.U8Vec1.html)
+/// * [`Vec1`](type.Vec1.html)
 pub type TVec1<N> = Vector1<N>;
 /// A 2D vector with components of type `N`.
+///
+/// # See also:
+///
+/// ## Constructors:
+///
+/// * [`make_vec2`](fn.make_vec2.html)
+/// * [`vec2`](fn.vec2.html)
+/// * [`vec1_to_vec2`](fn.vec1_to_vec2.html)
+/// * [`vec3_to_vec2`](fn.vec3_to_vec2.html)
+/// * [`vec4_to_vec2`](fn.vec4_to_vec2.html)
+///
+/// ## Related types:
+///
+/// * [`vec2`](fn.vec2.html)
+/// * [`BVec2`](type.BVec2.html)
+/// * [`DVec2`](type.DVec2.html)
+/// * [`IVec2`](type.IVec2.html)
+/// * [`I16Vec2`](type.I16Vec2.html)
+/// * [`I32Vec2`](type.I32Vec2.html)
+/// * [`I64Vec2`](type.I64Vec2.html)
+/// * [`I8Vec2`](type.I8Vec2.html)
+/// * [`TVec`](type.TVec.html)
+/// * [`UVec2`](type.UVec2.html)
+/// * [`U16Vec2`](type.U16Vec2.html)
+/// * [`U32Vec2`](type.U32Vec2.html)
+/// * [`U64Vec2`](type.U64Vec2.html)
+/// * [`U8Vec2`](type.U8Vec2.html)
+/// * [`Vec2`](type.Vec2.html)
 pub type TVec2<N> = Vector2<N>;
 /// A 3D vector with components of type `N`.
+///
+/// # See also:
+///
+/// ## Constructors:
+///
+/// * [`make_vec3`](fn.make_vec3.html)
+/// * [`vec3`](fn.vec3.html)
+/// * [`vec1_to_vec3`](fn.vec1_to_vec3.html)
+/// * [`vec2_to_vec3`](fn.vec2_to_vec3.html)
+/// * [`vec4_to_vec3`](fn.vec4_to_vec3.html)
+///
+/// ## Related types:
+///
+/// * [`vec3`](fn.vec3.html)
+/// * [`BVec3`](type.BVec3.html)
+/// * [`DVec3`](type.DVec3.html)
+/// * [`IVec3`](type.IVec3.html)
+/// * [`I16Vec3`](type.I16Vec3.html)
+/// * [`I32Vec3`](type.I32Vec3.html)
+/// * [`I64Vec3`](type.I64Vec3.html)
+/// * [`I8Vec3`](type.I8Vec3.html)
+/// * [`TVec`](type.TVec.html)
+/// * [`UVec3`](type.UVec3.html)
+/// * [`U16Vec3`](type.U16Vec3.html)
+/// * [`U32Vec3`](type.U32Vec3.html)
+/// * [`U64Vec3`](type.U64Vec3.html)
+/// * [`U8Vec3`](type.U8Vec3.html)
+/// * [`Vec3`](type.Vec3.html)
 pub type TVec3<N> = Vector3<N>;
 /// A 4D vector with components of type `N`.
+///
+/// # See also:
+///
+/// ## Constructors:
+///
+/// * [`make_vec4`](fn.make_vec4.html)
+/// * [`vec4`](fn.vec4.html)
+/// * [`vec1_to_vec4`](fn.vec1_to_vec4.html)
+/// * [`vec2_to_vec4`](fn.vec2_to_vec4.html)
+/// * [`vec3_to_vec4`](fn.vec3_to_vec4.html)
+///
+/// ## Related types:
+///
+/// * [`vec4`](fn.vec4.html)
+/// * [`BVec4`](type.BVec4.html)
+/// * [`DVec4`](type.DVec4.html)
+/// * [`IVec4`](type.IVec4.html)
+/// * [`I16Vec4`](type.I16Vec4.html)
+/// * [`I32Vec4`](type.I32Vec4.html)
+/// * [`I64Vec4`](type.I64Vec4.html)
+/// * [`I8Vec4`](type.I8Vec4.html)
+/// * [`UVec4`](type.UVec4.html)
+/// * [`U16Vec4`](type.U16Vec4.html)
+/// * [`U32Vec4`](type.U32Vec4.html)
+/// * [`U64Vec4`](type.U64Vec4.html)
+/// * [`U8Vec4`](type.U8Vec4.html)
+/// * [`Vec4`](type.Vec4.html)
 pub type TVec4<N> = Vector4<N>;
 /// A 1D vector with boolean components.
 pub type BVec1 = Vector1<bool>;
