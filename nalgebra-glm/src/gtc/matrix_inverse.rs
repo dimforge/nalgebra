cb96cdb79499724b9c327c8b1b59a28cc352e17e
Bruce Mitchener <bruce.mitchener@gmail.com>
2018-10-05 10:21:38 +0700

    clippy: Prefer unwrap_or_else to unwrap_or.

--- nalgebra-glm/src/gtc/matrix_inverse.rs
+++ nalgebra-glm/src/gtc/matrix_inverse.rs
@@ -7,11 +7,11 @@ use aliases::TMat;
 pub fn affine_inverse<N: Real, D: Dimension>(m: TMat<N, D, D>) -> TMat<N, D, D>
     where DefaultAllocator: Alloc<N, D, D> {
     // FIXME: this should be optimized.
-    m.try_inverse().unwrap_or(TMat::<_, D, D>::zeros())
+    m.try_inverse().unwrap_or_else(TMat::<_, D, D>::zeros)
 }
 
 /// Compute the transpose of the inverse of a matrix.
 pub fn inverse_transpose<N: Real, D: Dimension>(m: TMat<N, D, D>) -> TMat<N, D, D>
     where DefaultAllocator: Alloc<N, D, D> {
-    m.try_inverse().unwrap_or(TMat::<_, D, D>::zeros()).transpose()
+    m.try_inverse().unwrap_or_else(TMat::<_, D, D>::zeros).transpose()
 }
