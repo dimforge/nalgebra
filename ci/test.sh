35475634749de8e86244ff473d56f5cb1581b6d1
sebcrozet <developer@crozet.re>
2018-10-07 06:09:46 +0200

    Run CI tests on nalgebra-glm too.

--- ci/test.sh
+++ ci/test.sh
@@ -7,6 +7,7 @@ if [ -z "$NO_STD" ]; then
         cargo test --verbose;
         cargo test --verbose "arbitrary";
         cargo test --verbose "debug arbitrary mint serde-serialize abomonation-serialize";
+        cd nalgebra-glm; cargo test --verbose;
     else
         cd nalgebra-lapack; cargo test --verbose;
     fi
