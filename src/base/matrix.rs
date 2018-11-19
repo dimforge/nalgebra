267d9760bd8cac4572a5e5eb1203c7a1ce688ee3
Jim Turner <git@turner.link>
2018-11-19 16:51:01 -0500

    Add as_ptr and as_mut_ptr methods
    This is useful for moving around a matrix/slice in unsafe code and for
    converting slices to other types (e.g. `ndarray::ArrayView`).

--- src/base/matrix.rs
+++ src/base/matrix.rs
@@ -274,6 +274,15 @@ impl<N: Scalar, R: Dim, C: Dim, S: Storage<N, R, C>> Matrix<N, R, C, S> {
         self.data.get_unchecked(irow, icol)
     }
 
+    /// Returns a pointer to the start of the matrix.
+    ///
+    /// If the matrix is not empty, this pointer is guaranteed to be aligned
+    /// and non-null.
+    #[inline]
+    pub fn as_ptr(&self) -> *const N {
+        self.data.ptr()
+    }
+
     /// Tests whether `self` and `rhs` are equal up to a given epsilon.
     ///
     /// See `relative_eq` from the `RelativeEq` trait for more details.
@@ -548,6 +557,15 @@ impl<N: Scalar, R: Dim, C: Dim, S: StorageMut<N, R, C>> Matrix<N, R, C, S> {
     #[inline]
     pub fn iter_mut(&mut self) -> MatrixIterMut<N, R, C, S> {
         MatrixIterMut::new(&mut self.data)
+    }
+
+    /// Returns a mutable pointer to the start of the matrix.
+    ///
+    /// If the matrix is not empty, this pointer is guaranteed to be aligned
+    /// and non-null.
+    #[inline]
+    pub fn as_mut_ptr(&mut self) -> *mut N {
+        self.data.ptr_mut()
     }
 
     /// Gets a mutable reference to the i-th element of this matrix.
