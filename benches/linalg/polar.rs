062fb10372fe6e4d7376da4c90dd937bf41153ec
LCC <lcc@lcc.com>
2019-09-27 03:00:20 +0200

    Added benchmarks

---
+++ benches/linalg/polar.rs
@@ -0,0 +1,101 @@
+use na::{Matrix4, Polar};
+
+fn polar_decompose_4x4(bh: &mut criterion::Criterion) {
+    let m = Matrix4::<f64>::new_random();
+    bh.bench_function("polar_decompose_4x4", move |bh| bh.iter(|| test::black_box(Polar::new(m.clone(), true, true))));
+}
+
+fn polar_decompose_10x10(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(10, 10);
+    bh.bench_function("polar_decompose_10x10", move |bh| bh.iter(|| test::black_box(Polar::new(m.clone(), true, true))));
+}
+
+fn polar_decompose_100x100(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(100, 100);
+    bh.bench_function("polar_decompose_100x100", move |bh| bh.iter(|| test::black_box(Polar::new(m.clone(), true, true))));
+}
+
+fn polar_decompose_200x200(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(200, 200);
+    bh.bench_function("polar_decompose_200x200", move |bh| bh.iter(|| test::black_box(Polar::new(m.clone(), true, true))));
+}
+
+fn rank_4x4(bh: &mut criterion::Criterion) {
+    let m = Matrix4::<f64>::new_random();
+    bh.bench_function("rank_4x4", move |bh| bh.iter(|| test::black_box(m.rank(1.0e-10))));
+}
+
+fn rank_10x10(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(10, 10);
+    bh.bench_function("rank_10x10", move |bh| bh.iter(|| test::black_box(m.rank(1.0e-10))));
+}
+
+fn rank_100x100(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(100, 100);
+    bh.bench_function("rank_100x100", move |bh| bh.iter(|| test::black_box(m.rank(1.0e-10))));
+}
+
+fn rank_200x200(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(200, 200);
+    bh.bench_function("rank_200x200", move |bh| bh.iter(|| test::black_box(m.rank(1.0e-10))));
+}
+
+fn singular_values_4x4(bh: &mut criterion::Criterion) {
+    let m = Matrix4::<f64>::new_random();
+    bh.bench_function("singular_values_4x4", move |bh| bh.iter(|| test::black_box(m.singular_values())));
+}
+
+fn singular_values_10x10(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(10, 10);
+    bh.bench_function("singular_values_10x10", move |bh| bh.iter(|| test::black_box(m.singular_values())));
+}
+
+fn singular_values_100x100(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(100, 100);
+    bh.bench_function("singular_values_100x100", move |bh| bh.iter(|| test::black_box(m.singular_values())));
+}
+
+fn singular_values_200x200(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(200, 200);
+    bh.bench_function("singular_values_200x200", move |bh| bh.iter(|| test::black_box(m.singular_values())));
+}
+
+fn pseudo_inverse_4x4(bh: &mut criterion::Criterion) {
+    let m = Matrix4::<f64>::new_random();
+    bh.bench_function("pseudo_inverse_4x4", move |bh| bh.iter(|| test::black_box(m.clone().pseudo_inverse(1.0e-10))));
+}
+
+fn pseudo_inverse_10x10(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(10, 10);
+    bh.bench_function("pseudo_inverse_10x10", move |bh| bh.iter(|| test::black_box(m.clone().pseudo_inverse(1.0e-10))));
+}
+
+fn pseudo_inverse_100x100(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(100, 100);
+    bh.bench_function("pseudo_inverse_100x100", move |bh| bh.iter(|| test::black_box(m.clone().pseudo_inverse(1.0e-10))));
+}
+
+fn pseudo_inverse_200x200(bh: &mut criterion::Criterion) {
+    let m = crate::reproductible_dmatrix(200, 200);
+    bh.bench_function("pseudo_inverse_200x200", move |bh| bh.iter(|| test::black_box(m.clone().pseudo_inverse(1.0e-10))));
+}
+
+
+criterion_group!(polar,
+    polar_decompose_4x4,
+    polar_decompose_10x10,
+    polar_decompose_100x100,
+    polar_decompose_200x200,
+    rank_4x4,
+    rank_10x10,
+    rank_100x100,
+    rank_200x200,
+    singular_values_4x4,
+    singular_values_10x10,
+    singular_values_100x100,
+    singular_values_200x200,
+    pseudo_inverse_4x4,
+    pseudo_inverse_10x10,
+    pseudo_inverse_100x100,
+    pseudo_inverse_200x200
+);
