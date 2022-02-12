10f6c4867752fc9d1a59bee891e3efde70736ea9
metric-space <functor.soup@gmail.com>
2022-02-12 02:42:13 -0500

    Change name of test generator

--- nalgebra-lapack/tests/linalg/qz.rs
+++ nalgebra-lapack/tests/linalg/qz.rs
@@ -5,13 +5,15 @@ use crate::proptest::*;
 use proptest::{prop_assert, prop_compose, proptest};
 
 prop_compose! {
-fn f64_squares() (n in PROPTEST_MATRIX_DIM) (a in matrix(PROPTEST_F64,n,n), b in matrix(PROPTEST_F64,n,n)) -> (DMatrix<f64>, DMatrix<f64>){
+    fn f64_dynamic_dim_squares()
+        (n in PROPTEST_MATRIX_DIM)
+        (a in matrix(PROPTEST_F64,n,n), b in matrix(PROPTEST_F64,n,n)) -> (DMatrix<f64>, DMatrix<f64>){
     (a,b)
 }}
 
 proptest! {
     #[test]
-    fn qz((a,b) in f64_squares()) {
+    fn qz((a,b) in f64_dynamic_dim_squares()) {
 
         let qz = QZ::new(a.clone(), b.clone());
         let (vsl,s,t,vsr) = qz.clone().unpack();
