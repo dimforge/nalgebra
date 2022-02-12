fd0398f4935bb0238fb366b904afd28dc8ef173a
metric-space <functor.soup@gmail.com>
2022-02-12 02:27:29 -0500

    Remove condition number, tests pass without. Add proper test generator for dynamic f64 type square matrices

--- nalgebra-lapack/tests/linalg/generalized_eigenvalues.rs
+++ nalgebra-lapack/tests/linalg/generalized_eigenvalues.rs
@@ -1,74 +1,70 @@
-use na::dimension::{Const, Dynamic};
-use na::{DMatrix, EuclideanNorm, Norm, OMatrix};
+use na::dimension::Const;
+use na::{DMatrix, OMatrix};
 use nl::GE;
 use num_complex::Complex;
 use simba::scalar::ComplexField;
-use std::cmp;
 
 use crate::proptest::*;
-use proptest::{prop_assert, proptest};
+use proptest::{prop_assert, prop_compose, proptest};
+
+prop_compose! {
+fn f64_squares() (n in PROPTEST_MATRIX_DIM) (a in matrix(PROPTEST_F64,n,n), b in matrix(PROPTEST_F64,n,n)) -> (DMatrix<f64>, DMatrix<f64>){
+    (a,b)
+}}
 
 proptest! {
     #[test]
-    fn ge(n in PROPTEST_MATRIX_DIM) {
-        let n = cmp::max(1, cmp::min(n, 10));
-        let a = DMatrix::<f64>::new_random(n, n);
-        let b = DMatrix::<f64>::new_random(n, n);
-        let a_condition_no = a.clone().try_inverse().and_then(|x| Some(EuclideanNorm.norm(&x)* EuclideanNorm.norm(&a)));
-        let b_condition_no = b.clone().try_inverse().and_then(|x| Some(EuclideanNorm.norm(&x)* EuclideanNorm.norm(&b)));
+    fn ge((a,b) in f64_squares()){
 
-        if a_condition_no.unwrap_or(200000.0) < 5.0 && b_condition_no.unwrap_or(200000.0) < 5.0 {
-            let a_c = a.clone().map(|x| Complex::new(x, 0.0));
-            let b_c = b.clone().map(|x| Complex::new(x, 0.0));
+        let a_c = a.clone().map(|x| Complex::new(x, 0.0));
+        let b_c = b.clone().map(|x| Complex::new(x, 0.0));
+        let n = a.shape_generic().0;
 
-            let ge = GE::new(a.clone(), b.clone());
-            let (vsl,vsr) = ge.clone().eigenvectors();
+        let ge = GE::new(a.clone(), b.clone());
+        let (vsl,vsr) = ge.clone().eigenvectors();
 
-            for (i,(alpha,beta)) in ge.raw_eigenvalues().iter().enumerate() {
-                let l_a = a_c.clone() * Complex::new(*beta, 0.0);
-                let l_b = b_c.clone() * *alpha;
 
-                prop_assert!(
-                    relative_eq!(
-                        ((&l_a - &l_b)*vsr.column(i)).map(|x| x.modulus()),
-                        OMatrix::zeros_generic(Dynamic::new(n), Const::<1>),
-                        epsilon = 1.0e-7));
+        for (i,(alpha,beta)) in ge.raw_eigenvalues().iter().enumerate() {
+            let l_a = a_c.clone() * Complex::new(*beta, 0.0);
+            let l_b = b_c.clone() * *alpha;
 
-                prop_assert!(
-                    relative_eq!(
-                        (vsl.column(i).adjoint()*(&l_a - &l_b)).map(|x| x.modulus()),
-                        OMatrix::zeros_generic(Const::<1>, Dynamic::new(n)),
-                        epsilon = 1.0e-7))
-            };
+            prop_assert!(
+                relative_eq!(
+                    ((&l_a - &l_b)*vsr.column(i)).map(|x| x.modulus()),
+                    OMatrix::zeros_generic(n, Const::<1>),
+                    epsilon = 1.0e-5));
+
+            prop_assert!(
+                relative_eq!(
+                    (vsl.column(i).adjoint()*(&l_a - &l_b)).map(|x| x.modulus()),
+                    OMatrix::zeros_generic(Const::<1>, n),
+                    epsilon = 1.0e-5))
         };
     }
 
     #[test]
     fn ge_static(a in matrix4(), b in matrix4()) {
-        let a_condition_no = a.clone().try_inverse().and_then(|x| Some(EuclideanNorm.norm(&x)* EuclideanNorm.norm(&a)));
-        let b_condition_no = b.clone().try_inverse().and_then(|x| Some(EuclideanNorm.norm(&x)* EuclideanNorm.norm(&b)));
 
-        if a_condition_no.unwrap_or(200000.0) < 5.0 && b_condition_no.unwrap_or(200000.0) < 5.0 {
-            let ge = GE::new(a.clone(), b.clone());
-            let a_c =a.clone().map(|x| Complex::new(x, 0.0));
-            let b_c = b.clone().map(|x| Complex::new(x, 0.0));
-            let (vsl,vsr) = ge.eigenvectors();
-            let eigenvalues = ge.raw_eigenvalues();
+        let ge = GE::new(a.clone(), b.clone());
+        let a_c =a.clone().map(|x| Complex::new(x, 0.0));
+        let b_c = b.clone().map(|x| Complex::new(x, 0.0));
+        let (vsl,vsr) = ge.eigenvectors();
+        let eigenvalues = ge.raw_eigenvalues();
 
-            for (i,(alpha,beta)) in eigenvalues.iter().enumerate() {
-                let l_a = a_c.clone() * Complex::new(*beta, 0.0);
-                let l_b = b_c.clone() * *alpha;
+        for (i,(alpha,beta)) in eigenvalues.iter().enumerate() {
+            let l_a = a_c.clone() * Complex::new(*beta, 0.0);
+            let l_b = b_c.clone() * *alpha;
 
-                prop_assert!(
-                    relative_eq!(
-                        ((&l_a - &l_b)*vsr.column(i)).map(|x| x.modulus()),
-                        OMatrix::zeros_generic(Const::<4>, Const::<1>),
-                        epsilon = 1.0e-7));
-                prop_assert!(
-                    relative_eq!((vsl.column(i).adjoint()*(&l_a - &l_b)).map(|x| x.modulus()),
-                                 OMatrix::zeros_generic(Const::<1>, Const::<4>),
-                                 epsilon = 1.0e-7))
-            }
-        };
+            prop_assert!(
+                relative_eq!(
+                    ((&l_a - &l_b)*vsr.column(i)).map(|x| x.modulus()),
+                    OMatrix::zeros_generic(Const::<4>, Const::<1>),
+                    epsilon = 1.0e-5));
+            prop_assert!(
+                relative_eq!((vsl.column(i).adjoint()*(&l_a - &l_b)).map(|x| x.modulus()),
+                             OMatrix::zeros_generic(Const::<1>, Const::<4>),
+                             epsilon = 1.0e-5))
+        }
     }
+
 }
