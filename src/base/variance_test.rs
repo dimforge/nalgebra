032002dce964391e016bf880c894993246aec6eb
Vasil Nikolov <vaskonikolov2003@gmail.com>
2023-04-24 01:22:57 +0300

    initial, unoptimized algoritm

--- src/base/variance_test.rs
+++ src/base/variance_test.rs
@@ -2,6 +2,6 @@
 use crate::DVector;
 #[test]
 fn test_variance_new() {
-    let v = DVector::repeat(10_000, 100000000.1234);
+    let v = DVector::repeat(10_000, 100000000.0);
     assert_eq!(v.variance(), 0.0)
 }
