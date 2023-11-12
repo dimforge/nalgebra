06b8d38970e984ab113d9f893b579fbc4ebb52ed
Sébastien Crozet <developer@crozet.re>
2023-11-12 23:17:17 +0100

    fix no-std builds.

--- src/base/vec_storage.rs
+++ src/base/vec_storage.rs
@@ -34,7 +34,7 @@ pub struct VecStorage<T, R: Dim, C: Dim> {
 impl<T> Default for VecStorage<T, Dyn, Dyn> {
     fn default() -> Self {
         Self {
-            data: vec![],
+            data: Vec::new(),
             nrows: Dyn::from_usize(0),
             ncols: Dyn::from_usize(0),
         }
@@ -44,7 +44,7 @@ impl<T> Default for VecStorage<T, Dyn, Dyn> {
 impl<T, R: DimName> Default for VecStorage<T, R, Dyn> {
     fn default() -> Self {
         Self {
-            data: vec![],
+            data: Vec::new(),
             nrows: R::name(),
             ncols: Dyn::from_usize(0),
         }
@@ -54,7 +54,7 @@ impl<T, R: DimName> Default for VecStorage<T, R, Dyn> {
 impl<T, C: DimName> Default for VecStorage<T, Dyn, C> {
     fn default() -> Self {
         Self {
-            data: vec![],
+            data: Vec::new(),
             nrows: Dyn::from_usize(0),
             ncols: C::name(),
         }
