12a20465a516a3df233fdf345a6ea26e189e5024
fangs124 <putraa@student.unimelb.edu.au>
2024-04-24 16:58:51 +0700

    removed (my own) comment

--- tests/core/matrix.rs
+++ tests/core/matrix.rs
@@ -924,7 +924,7 @@ mod transposition_tests {
             prop_assert!(relative_eq!(m.transpose() * v, m.tr_mul(&v), epsilon = 1.0e-7))
         }
 
-        #[test] //this one gives an error
+        #[test]
         fn mul_tr_is_transpose_rhs_then_mul(m in matrix(PROPTEST_F64, Const::<6>, Const::<4>), v in vector4()) {
             prop_assert!(relative_eq!(m * v, m.mul_tr(&v.transpose()), epsilon = 1.0e-7))
         }
