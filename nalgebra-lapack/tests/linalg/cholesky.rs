885d39c03b313f87759c538830b582890f0a6f3b
geo-ant <54497890+geo-ant@users.noreply.github.com>
2025-09-07 08:40:24 +0200

    improve cholesky tests for better output

--- nalgebra-lapack/tests/linalg/cholesky.rs
+++ nalgebra-lapack/tests/linalg/cholesky.rs
@@ -1,15 +1,37 @@
+use crate::proptest::*;
+use na::{Const, DMatrix, Matrix4, Matrix4xX};
+use nl::Cholesky;
+use proptest::prelude::*;
 use std::cmp;
 
-use na::{DMatrix, DVector, Matrix4x3, Vector4};
-use nl::Cholesky;
+// actually, cholesky needs positive DEFINITE matrices, but those
+// are a bit harder to generate, so this should do.
+fn positive_semidefinite_dmatrix() -> impl Strategy<Value = DMatrix<f64>> {
+    dmatrix().prop_map(|m| &m * m.transpose())
+}
 
-use crate::proptest::*;
-use proptest::{prop_assert, proptest};
+fn positive_semidefinite_matrix4() -> impl Strategy<Value = Matrix4<f64>> {
+    matrix4().prop_map(|m| &m * m.transpose())
+}
+
+fn positive_semidefinite_linear_system() -> impl Strategy<Value = (DMatrix<f64>, DMatrix<f64>)> {
+    positive_semidefinite_dmatrix().prop_flat_map(|a| {
+        let b = matrix(PROPTEST_F64, a.nrows(), PROPTEST_MATRIX_DIM);
+        (Just(a), b)
+    })
+}
+
+fn positive_semidefinite_linear_system_4() -> impl Strategy<Value = (Matrix4<f64>, Matrix4xX<f64>)>
+{
+    positive_semidefinite_matrix4().prop_flat_map(|a| {
+        let b = matrix(PROPTEST_F64, Const::<4>, PROPTEST_MATRIX_DIM);
+        (Just(a), b)
+    })
+}
 
 proptest! {
     #[test]
-    fn cholesky(m in dmatrix()) {
-        let m = &m * m.transpose();
+    fn cholesky(m in positive_semidefinite_dmatrix()) {
         if let Some(chol) = Cholesky::new(m.clone()) {
             let l = chol.unpack();
             let reconstructed_m = &l * l.transpose();
@@ -19,8 +41,7 @@ proptest! {
     }
 
     #[test]
-    fn cholesky_static(m in matrix3()) {
-        let m = &m * m.transpose();
+    fn cholesky_static(m in positive_semidefinite_matrix4()) {
         if let Some(chol) = Cholesky::new(m) {
             let l = chol.unpack();
             let reconstructed_m = &l * l.transpose();
@@ -30,36 +51,19 @@ proptest! {
     }
 
     #[test]
-    fn cholesky_solve(n in PROPTEST_MATRIX_DIM, nb in PROPTEST_MATRIX_DIM) {
-        let n  = cmp::min(n, 15);  // To avoid slowing down the test too much.
-        let nb = cmp::min(nb, 15); // To avoid slowing down the test too much.
-        let m  = DMatrix::<f64>::new_random(n, n);
-        let m   = &m * m.transpose();
+    fn cholesky_solve((a,b) in positive_semidefinite_linear_system()) {
 
-        if let Some(chol) = Cholesky::new(m.clone()) {
-            let b1 = DVector::new_random(n);
-            let b2 = DMatrix::new_random(n, nb);
-
-            let sol1 = chol.solve(&b1).unwrap();
-            let sol2 = chol.solve(&b2).unwrap();
-
-            prop_assert!(relative_eq!(&m * sol1, b1, epsilon = 1.0e-6));
-            prop_assert!(relative_eq!(&m * sol2, b2, epsilon = 1.0e-6));
+        if let Some(chol) = Cholesky::new(a.clone()) {
+            let sol = chol.solve(&b).unwrap();
+            prop_assert!(relative_eq!(&a * sol, b, epsilon = 1.0e-6));
         }
     }
 
     #[test]
-    fn cholesky_solve_static(m in matrix4()) {
-        let m = &m * m.transpose();
-        if let Some(chol) = Cholesky::new(m) {
-            let b1 = Vector4::new_random();
-            let b2 = Matrix4x3::new_random();
-
-            let sol1 = chol.solve(&b1).unwrap();
-            let sol2 = chol.solve(&b2).unwrap();
-
-            prop_assert!(relative_eq!(m * sol1, b1, epsilon = 1.0e-4));
-            prop_assert!(relative_eq!(m * sol2, b2, epsilon = 1.0e-4));
+    fn cholesky_solve_static((a,b) in positive_semidefinite_linear_system_4()) {
+        if let Some(chol) = Cholesky::new(a) {
+            let sol = chol.solve(&b).unwrap();
+            prop_assert!(relative_eq!(a * sol, b, epsilon = 1.0e-4));
         }
     }
 
