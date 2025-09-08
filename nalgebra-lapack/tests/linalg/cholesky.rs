a4eafb2895829dccc804b60276c6ed75437adf70
geo-ant <54497890+geo-ant@users.noreply.github.com>
2025-09-08 07:08:10 +0200

    make sure in cholesky tests to have strictly positive definite matrices

--- nalgebra-lapack/tests/linalg/cholesky.rs
+++ nalgebra-lapack/tests/linalg/cholesky.rs
@@ -1,29 +1,37 @@
 use crate::proptest::*;
+use core::f64;
 use na::{Const, DMatrix, Matrix4, Matrix4xX};
 use nl::Cholesky;
 use proptest::prelude::*;
 use std::cmp;
 
-// actually, cholesky needs positive DEFINITE matrices, but those
-// are a bit harder to generate, so this should do.
-fn positive_semidefinite_dmatrix() -> impl Strategy<Value = DMatrix<f64>> {
-    dmatrix().prop_map(|m| &m * m.transpose())
+fn positive_definite_dmatrix() -> impl Strategy<Value = DMatrix<f64>> {
+    // @note(geo-ant) to get positive definite matrices we use M*M^T + alpha*I,
+    // where alpha is a constant that is chosen so that the eigenvales stay
+    // positive.
+    dmatrix().prop_map(|m| {
+        let alpha = f64::EPSILON.sqrt() * m.norm_squared();
+        let nrows = m.nrows();
+        &m * m.transpose() + alpha * DMatrix::identity(nrows, nrows)
+    })
 }
 
-fn positive_semidefinite_matrix4() -> impl Strategy<Value = Matrix4<f64>> {
-    matrix4().prop_map(|m| &m * m.transpose())
+fn positive_definite_matrix4() -> impl Strategy<Value = Matrix4<f64>> {
+    matrix4().prop_map(|m| {
+        let alpha = f64::EPSILON.sqrt() * m.norm_squared();
+        &m * m.transpose() + alpha * Matrix4::identity()
+    })
 }
 
-fn positive_semidefinite_linear_system() -> impl Strategy<Value = (DMatrix<f64>, DMatrix<f64>)> {
-    positive_semidefinite_dmatrix().prop_flat_map(|a| {
+fn positive_definite_linear_system() -> impl Strategy<Value = (DMatrix<f64>, DMatrix<f64>)> {
+    positive_definite_dmatrix().prop_flat_map(|a| {
         let b = matrix(PROPTEST_F64, a.nrows(), PROPTEST_MATRIX_DIM);
         (Just(a), b)
     })
 }
 
-fn positive_semidefinite_linear_system_4() -> impl Strategy<Value = (Matrix4<f64>, Matrix4xX<f64>)>
-{
-    positive_semidefinite_matrix4().prop_flat_map(|a| {
+fn positive_definite_linear_system_4() -> impl Strategy<Value = (Matrix4<f64>, Matrix4xX<f64>)> {
+    positive_definite_matrix4().prop_flat_map(|a| {
         let b = matrix(PROPTEST_F64, Const::<4>, PROPTEST_MATRIX_DIM);
         (Just(a), b)
     })
@@ -31,7 +39,7 @@ fn positive_semidefinite_linear_system_4() -> impl Strategy<Value = (Matrix4<f64
 
 proptest! {
     #[test]
-    fn cholesky(m in positive_semidefinite_dmatrix()) {
+    fn cholesky(m in positive_definite_dmatrix()) {
         if let Some(chol) = Cholesky::new(m.clone()) {
             let l = chol.unpack();
             let reconstructed_m = &l * l.transpose();
@@ -41,7 +49,7 @@ proptest! {
     }
 
     #[test]
-    fn cholesky_static(m in positive_semidefinite_matrix4()) {
+    fn cholesky_static(m in positive_definite_matrix4()) {
         if let Some(chol) = Cholesky::new(m) {
             let l = chol.unpack();
             let reconstructed_m = &l * l.transpose();
@@ -51,7 +59,7 @@ proptest! {
     }
 
     #[test]
-    fn cholesky_solve((a,b) in positive_semidefinite_linear_system()) {
+    fn cholesky_solve((a,b) in positive_definite_linear_system()) {
 
         if let Some(chol) = Cholesky::new(a.clone()) {
             let sol = chol.solve(&b).unwrap();
@@ -60,7 +68,7 @@ proptest! {
     }
 
     #[test]
-    fn cholesky_solve_static((a,b) in positive_semidefinite_linear_system_4()) {
+    fn cholesky_solve_static((a,b) in positive_definite_linear_system_4()) {
         if let Some(chol) = Cholesky::new(a) {
             let sol = chol.solve(&b).unwrap();
             prop_assert!(relative_eq!(a * sol, b, epsilon = 1.0e-4));
