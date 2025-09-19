6519048f03fd562009d898e73f1f46304d5885f3
Ivan Mironov <mironov.ivan@gmail.com>
2025-09-19 20:46:12 +0200

    Merge Fix linalg bench warnings (#1544)
    * chore: `cargo check` all targets in CI
    
    * chore: Remove benches commented out as "too long"
    
    * chore: fix warning: unused return value of `LU::<T, D, D>::solve`
    
    This fixes following warning:
    
    	warning: unused return value of `LU::<T, D, D>::solve` that must be used
    	  --> benches/linalg/lu.rs:33:13
    	   |
    	33 |             lu.solve(&mut b);
    	   |             ^^^^^^^^^^^^^^^^
    	   |
    	   = note: Did you mean to use solve_mut()?
    	   = note: `#[warn(unused_must_use)]` on by default
    	help: use `let _ = ...` to ignore the resulting value
    	   |
    	33 |             let _ = lu.solve(&mut b);
    	   |             +++++++

--- benches/linalg/full_piv_lu.rs
+++ benches/linalg/full_piv_lu.rs
@@ -15,13 +15,6 @@ fn full_piv_lu_decompose_100x100(bh: &mut criterion::Criterion) {
     });
 }
 
-fn full_piv_lu_decompose_500x500(bh: &mut criterion::Criterion) {
-    let m = DMatrix::<f64>::new_random(500, 500);
-    bh.bench_function("full_piv_lu_decompose_500x500", move |bh| {
-        bh.iter(|| std::hint::black_box(FullPivLU::new(m.clone())))
-    });
-}
-
 fn full_piv_lu_solve_10x10(bh: &mut criterion::Criterion) {
     let m = DMatrix::<f64>::new_random(10, 10);
     let lu = FullPivLU::new(m.clone());
@@ -29,7 +22,8 @@ fn full_piv_lu_solve_10x10(bh: &mut criterion::Criterion) {
     bh.bench_function("full_piv_lu_solve_10x10", move |bh| {
         bh.iter(|| {
             let mut b = DVector::<f64>::from_element(10, 1.0);
-            lu.solve(&mut b);
+            lu.solve_mut(&mut b);
+            b
         })
     });
 }
@@ -41,19 +35,8 @@ fn full_piv_lu_solve_100x100(bh: &mut criterion::Criterion) {
     bh.bench_function("full_piv_lu_solve_100x100", move |bh| {
         bh.iter(|| {
             let mut b = DVector::<f64>::from_element(100, 1.0);
-            lu.solve(&mut b);
-        })
-    });
-}
-
-fn full_piv_lu_solve_500x500(bh: &mut criterion::Criterion) {
-    let m = DMatrix::<f64>::new_random(500, 500);
-    let lu = FullPivLU::new(m.clone());
-
-    bh.bench_function("full_piv_lu_solve_500x500", move |bh| {
-        bh.iter(|| {
-            let mut b = DVector::<f64>::from_element(500, 1.0);
-            lu.solve(&mut b);
+            lu.solve_mut(&mut b);
+            b
         })
     });
 }
@@ -76,15 +59,6 @@ fn full_piv_lu_inverse_100x100(bh: &mut criterion::Criterion) {
     });
 }
 
-fn full_piv_lu_inverse_500x500(bh: &mut criterion::Criterion) {
-    let m = DMatrix::<f64>::new_random(500, 500);
-    let lu = FullPivLU::new(m.clone());
-
-    bh.bench_function("full_piv_lu_inverse_500x500", move |bh| {
-        bh.iter(|| std::hint::black_box(lu.try_inverse()))
-    });
-}
-
 fn full_piv_lu_determinant_10x10(bh: &mut criterion::Criterion) {
     let m = DMatrix::<f64>::new_random(10, 10);
     let lu = FullPivLU::new(m.clone());
@@ -103,27 +77,14 @@ fn full_piv_lu_determinant_100x100(bh: &mut criterion::Criterion) {
     });
 }
 
-fn full_piv_lu_determinant_500x500(bh: &mut criterion::Criterion) {
-    let m = DMatrix::<f64>::new_random(500, 500);
-    let lu = FullPivLU::new(m.clone());
-
-    bh.bench_function("full_piv_lu_determinant_500x500", move |bh| {
-        bh.iter(|| std::hint::black_box(lu.determinant()))
-    });
-}
-
 criterion_group!(
     full_piv_lu,
     full_piv_lu_decompose_10x10,
     full_piv_lu_decompose_100x100,
-    //    full_piv_lu_decompose_500x500,
     full_piv_lu_solve_10x10,
     full_piv_lu_solve_100x100,
-    //    full_piv_lu_solve_500x500,
     full_piv_lu_inverse_10x10,
     full_piv_lu_inverse_100x100,
-    //    full_piv_lu_inverse_500x500,
     full_piv_lu_determinant_10x10,
     full_piv_lu_determinant_100x100,
-    //    full_piv_lu_determinant_500x500
 );
--- benches/linalg/lu.rs
+++ benches/linalg/lu.rs
@@ -15,13 +15,6 @@ fn lu_decompose_100x100(bh: &mut criterion::Criterion) {
     });
 }
 
-fn lu_decompose_500x500(bh: &mut criterion::Criterion) {
-    let m = DMatrix::<f64>::new_random(500, 500);
-    bh.bench_function("lu_decompose_500x500", move |bh| {
-        bh.iter(|| std::hint::black_box(LU::new(m.clone())))
-    });
-}
-
 fn lu_solve_10x10(bh: &mut criterion::Criterion) {
     let m = DMatrix::<f64>::new_random(10, 10);
     let lu = LU::new(m.clone());
@@ -29,7 +22,8 @@ fn lu_solve_10x10(bh: &mut criterion::Criterion) {
     bh.bench_function("lu_solve_10x10", move |bh| {
         bh.iter(|| {
             let mut b = DVector::<f64>::from_element(10, 1.0);
-            lu.solve(&mut b);
+            lu.solve_mut(&mut b);
+            b
         })
     });
 }
@@ -41,19 +35,8 @@ fn lu_solve_100x100(bh: &mut criterion::Criterion) {
     bh.bench_function("lu_solve_100x100", move |bh| {
         bh.iter(|| {
             let mut b = DVector::<f64>::from_element(100, 1.0);
-            lu.solve(&mut b);
-        })
-    });
-}
-
-fn lu_solve_500x500(bh: &mut criterion::Criterion) {
-    let m = DMatrix::<f64>::new_random(500, 500);
-    let lu = LU::new(m.clone());
-
-    bh.bench_function("", move |bh| {
-        bh.iter(|| {
-            let mut b = DVector::<f64>::from_element(500, 1.0);
-            lu.solve(&mut b);
+            lu.solve_mut(&mut b);
+            b
         })
     });
 }
@@ -76,15 +59,6 @@ fn lu_inverse_100x100(bh: &mut criterion::Criterion) {
     });
 }
 
-fn lu_inverse_500x500(bh: &mut criterion::Criterion) {
-    let m = DMatrix::<f64>::new_random(500, 500);
-    let lu = LU::new(m.clone());
-
-    bh.bench_function("lu_inverse_500x500", move |bh| {
-        bh.iter(|| std::hint::black_box(lu.try_inverse()))
-    });
-}
-
 fn lu_determinant_10x10(bh: &mut criterion::Criterion) {
     let m = DMatrix::<f64>::new_random(10, 10);
     let lu = LU::new(m.clone());
@@ -103,25 +77,14 @@ fn lu_determinant_100x100(bh: &mut criterion::Criterion) {
     });
 }
 
-fn lu_determinant_500x500(bh: &mut criterion::Criterion) {
-    let m = DMatrix::<f64>::new_random(500, 500);
-    let lu = LU::new(m.clone());
-
-    bh.bench_function("", move |bh| {
-        bh.iter(|| std::hint::black_box(lu.determinant()))
-    });
-}
-
 criterion_group!(
     lu,
     lu_decompose_10x10,
     lu_decompose_100x100,
-    //    lu_decompose_500x500,
     lu_solve_10x10,
     lu_solve_100x100,
     lu_inverse_10x10,
     lu_inverse_100x100,
-    //    lu_inverse_500x500,
     lu_determinant_10x10,
-    lu_determinant_100x100
+    lu_determinant_100x100,
 );
